dbbd35a7b5c1f482fdc199b7ac57a5023411e2cc
siddh34 <ssiddharthsutar@gmail.com>
2024-07-01 00:10:32 +0530

    added proper logs

--- test/test_fun.py
+++ test/test_fun.py
@@ -5,12 +5,15 @@ from fun import add
 
 class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
+        print("Testing addition of positive numbers")
         self.assertEqual(add(2, 3), 5)
 
     def test_add_negative_numbers(self):
+        print("Testing addition of negative numbers")
         self.assertEqual(add(-1, -1), -2)
 
     def test_add_positive_and_negative_number(self):
+        print("Testing addition of integers")
         self.assertEqual(add(-1, 2), 1)
 
 if __name__ == '__main__':
