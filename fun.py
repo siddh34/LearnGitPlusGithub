7a27e066f51c0492146de82bb58daa47a63e71dc
siddh34 <ssiddharthsutar@gmail.com>
2024-07-03 14:06:18 +0530

    corrected code

--- fun.py
+++ fun.py
@@ -1,7 +1,7 @@
 # Complete function to calculate the sum of two numbers
 
 def add(a: int, b: int) -> int:
-    return a - b
+    return a + b
 
 def sub(a: int,b: int) -> int:
-    return a + b
+    return a - b
